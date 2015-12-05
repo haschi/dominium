ba4ca9d3e9ac246d1c1e08ad2d4edff9b741fc01
matthias <matthias.haschka@gmail.com>
2015-12-05 19:24:49 +0100

    Dokumentation

---
+++ domaene/src/test/java/de/therapeutenkiller/haushaltsbuch/domaene/KassenbonErhaltenSteps.java
@@ -0,0 +1,15 @@
+package de.therapeutenkiller.haushaltsbuch.domaene;
+
+import cucumber.api.java.en.Given;
+
+public class KassenbonErhaltenSteps {
+
+    public KassenbonErhaltenSteps() {
+        // Der Konstruktor ist absichtlich leer wegen weiß ich nicht        
+    }
+
+    @Given("^mein Vermögen beträgt (\\d+) €$")
+    public void given_mein_vermögen_beträgt(final Integer betrag) {
+
+    }
+}
