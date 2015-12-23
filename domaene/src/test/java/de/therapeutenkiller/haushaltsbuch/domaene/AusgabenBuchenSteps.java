530f9f5ffd09149797dd24904358451cce5ce12f
matthias <matthias.haschka@gmail.com>
2015-12-23 12:09:28 +0100

    Korrektur Bilder

---
+++ domaene/src/test/java/de/therapeutenkiller/haushaltsbuch/domaene/AusgabenBuchenSteps.java
@@ -0,0 +1,16 @@
+package de.therapeutenkiller.haushaltsbuch.domaene;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.de.Angenommen;
+
+/**
+ * Created by matthias on 23.12.15.
+ */
+public class AusgabenBuchenSteps {
+    @Angenommen("^ich mein Haushaltsbuch besitzt folgende Konten:$")
+    public void ichMeinHaushaltsbuchBesitztFolgendeKonten(DataTable konten) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
