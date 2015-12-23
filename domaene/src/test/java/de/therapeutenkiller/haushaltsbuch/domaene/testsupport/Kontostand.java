805d6351201bde4336a0131b86627eac0ec2c2ea
matthias <matthias.haschka@gmail.com>
2015-12-23 12:09:46 +0100

    Korrektur Bilder

---
+++ domaene/src/test/java/de/therapeutenkiller/haushaltsbuch/domaene/testsupport/Kontostand.java
@@ -0,0 +1,15 @@
+package de.therapeutenkiller.haushaltsbuch.domaene.testsupport;
+
+import de.therapeutenkiller.haushaltsbuch.domaene.aggregat.Konto;
+
+import javax.money.MonetaryAmount;
+
+/**
+ * Created by matthias on 23.12.15.
+ */
+public class Kontostand {
+
+    Konto konto;
+    MonetaryAmount betrag;
+
+}
