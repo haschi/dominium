398e426a524f662fd7b9fc0882e43be3a3c9c53f
matthias <matthias.haschka@gmail.com>
2016-01-17 11:11:57 +0100

    Spike Aggregatwurzel: Aggregatwurzel speichert Ereignis und Version

--- infrastruktur/src/test/groovy/de/therapeutenkiller/haushaltsbuch/persistenz/AggregatwurzelTest.groovy
+++ infrastruktur/src/test/groovy/de/therapeutenkiller/haushaltsbuch/persistenz/AggregatwurzelTest.groovy
@@ -1,11 +1,15 @@
 package de.therapeutenkiller.haushaltsbuch.persistenz
 
+import de.therapeutenkiller.haushaltsbuch.domaene.support.Wertobjekt
 import de.therapeutenkiller.haushaltsbuch.persistenz.testdomaene.Aggregatwurzel
+import de.therapeutenkiller.haushaltsbuch.persistenz.testdomaene.Domänenereignis
 import spock.lang.Specification
 
 class AggregatwurzelTest extends Specification {
 
     class TestAggregat extends Aggregatwurzel<TestAggregat, UUID> {
+
+        long zustand
 
         protected TestAggregat(UUID identitätsmerkmal) {
             super(identitätsmerkmal)
@@ -14,6 +18,29 @@ class AggregatwurzelTest extends Specification {
         @Override
         protected TestAggregat getSelf() {
             return this
+        }
+
+        public void zustandÄndern(long payload) {
+            bewirkt( new ZustandWurdeGeändert(payload))
+        }
+
+        void falls(ZustandWurdeGeändert zustandWurdeGeändert) {
+            this.zustand = zustandWurdeGeändert.payload
+        }
+    }
+
+    class ZustandWurdeGeändert extends  Wertobjekt implements Domänenereignis<TestAggregat> {
+
+        private final long payload
+
+        ZustandWurdeGeändert(long payload) {
+
+            this.payload = payload
+        }
+
+        @Override
+        void anwendenAuf(TestAggregat aggregat) {
+            aggregat.falls(this)
         }
     }
 
@@ -27,5 +54,47 @@ class AggregatwurzelTest extends Specification {
 
         then: "werde ich ein Aggregat mit Identitätsmerkmal erhalten"
         aggregat.identitätsmerkmal == identitätsmerkmal;
+    }
+
+    def "Eine Aggregatwurzel merkt sich ein aufgetretenes Ereignisse"() {
+
+        given: "ich habe eine Aggregatwurzel"
+        TestAggregat aggregat = new TestAggregat(UUID.randomUUID());
+
+        when: "ich eine Änderung am Aggregat durchführe"
+        aggregat.zustandÄndern(42L)
+
+        then: "wird die Zustandsänderung gemerkt"
+        aggregat.änderungen.contains(new ZustandWurdeGeändert(42L))
+    }
+
+    def "Eine Aggregatwurzel merkt sich Ereignisse in der Reihenfolge, in der sie auftreten"() {
+
+        given: "ich habe eine Aggregatwurzel"
+        TestAggregat aggregat = new TestAggregat(UUID.randomUUID())
+
+        when: "ich mehrere Änderungen am Aggregat durchführe"
+        aggregat.zustandÄndern 42L
+        aggregat.zustandÄndern 43L
+
+        then:
+        aggregat.änderungen == [new ZustandWurdeGeändert(42L), new ZustandWurdeGeändert(43L)]
+    }
+
+    def "Eine Aggregatwurzel führt Änderungen am Zustand sofort durch"() {
+
+        given: "ich habe eine Aggregatwurzel"
+        TestAggregat aggregat = new TestAggregat(UUID.randomUUID())
+
+        when: "ich eine Zustandsänderung durchführe"
+        aggregat.zustandÄndern(42L)
+
+        then: "wird das Aggregat den neuen Zustand besitzen"
+        aggregat.zustand == 42L
+    }
+
+    def "Ein neues Aggregat besitzt die Version 0"() {
+        expect:
+        new TestAggregat(UUID.randomUUID()).version == 0
     }
 }
--- infrastruktur/src/test/java/de/therapeutenkiller/haushaltsbuch/persistenz/testdomaene/Aggregatwurzel.java
+++ infrastruktur/src/test/java/de/therapeutenkiller/haushaltsbuch/persistenz/testdomaene/Aggregatwurzel.java
@@ -2,16 +2,33 @@ package de.therapeutenkiller.haushaltsbuch.persistenz.testdomaene;
 
 import de.therapeutenkiller.haushaltsbuch.domaene.support.Entität;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Aggregatwurzel<A extends Aggregatwurzel<A, T>, T> extends Entität<T> {
+
+    private List<Domänenereignis<A>> änderungen;
+    private long version;
 
     protected Aggregatwurzel(final T identitätsmerkmal) {
         super(identitätsmerkmal);
+        this.änderungen = new ArrayList<>();
+        this.version = 0L;
     }
 
     protected final void bewirkt(final Domänenereignis<A> ereignis) {
+        this.änderungen.add(ereignis);
         ereignis.anwendenAuf(this.getSelf());
     }
 
+    public final List<Domänenereignis<A>> getÄnderungen() {
+        return this.änderungen;
+    }
+
     protected abstract A getSelf();
+
+    public final long getVersion() {
+        return this.version;
+    }
 }
 
