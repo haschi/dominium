147c928a0e775d565dad78677893cf8f0821ecc9
matthias <matthias.haschka@gmail.com>
2016-05-28 12:31:52 +0200

    Ereignisquelle: Ereignisse werde an die Abonnenten weitergeleitet

--- dominium/src/main/java/com/github/haschi/dominium/modell/Ereignisquelle.java
+++ dominium/src/main/java/com/github/haschi/dominium/modell/Ereignisquelle.java
@@ -1,8 +1,12 @@
 package com.github.haschi.dominium.modell;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Ereignisquelle<T> {
 
     private Version version;
+    private Set<T> abonnenten = new HashSet<>();
 
     public Ereignisquelle() {
         this(Version.NEU);
@@ -14,10 +18,15 @@ public class Ereignisquelle<T> {
     }
 
     public void bewirkt(final Domänenereignis<T> ereignis) {
+        abonnenten.forEach(ereignis::anwendenAuf);
         this.version = this.version.nachfolger();
     }
 
     public Version getVersion() {
         return this.version;
+    }
+
+    public void abonnieren(final T abonnent) {
+        this.abonnenten.add(abonnent);
     }
 }
--- dominium/src/test/groovy/com/github/haschi/dominium/modell/EreignisquelleTest.groovy
+++ dominium/src/test/groovy/com/github/haschi/dominium/modell/EreignisquelleTest.groovy
@@ -6,14 +6,15 @@ import spock.lang.Specification
 
 class EreignisquelleTest extends Specification {
 
+    public static final long PAYLOAD = 42L
+
     def "Ereignisse der Quelle hinzufügen erhöht die Version"() {
 
         given: "ich habe eine neue Ereignisquelle"
-        UUID identitätsmerkmal = UUID.randomUUID()
         Ereignisquelle<TestAggregatEreignisZiel> ereignisquelle = new Ereignisquelle<>(Version.NEU.nachfolger());
 
         when: "ich der Quelle ein Ereignis auslöst"
-        ereignisquelle.bewirkt(ZustandWurdeGeändert.of(42L))
+        ereignisquelle.bewirkt(ZustandWurdeGeändert.of(PAYLOAD))
 
         then: "wird die Versionsnummer des Aggregats in der Ereignisquelle erhöht"
         ereignisquelle.getVersion() == Version.NEU.nachfolger().nachfolger()
@@ -22,5 +23,22 @@ class EreignisquelleTest extends Specification {
     def "Eine neue Ereignisquelle besitzt die Anfangsversion"() {
         expect:
         new Ereignisquelle<TestAggregatEreignisZiel>().version == Version.NEU
+    }
+
+    def "Ereignisse werde an die Abonnenten weitergeleitet"() {
+        given: "ich habe eine Ereignisquelle mit Abonnenten"
+        Ereignisquelle<TestAggregatEreignisZiel> ereignisquelle = new Ereignisquelle<>()
+        def abonnent1 = Mock(TestAggregatEreignisZiel)
+        def abonnent2 = Mock(TestAggregatEreignisZiel)
+
+        ereignisquelle.abonnieren(abonnent1)
+        ereignisquelle.abonnieren(abonnent2)
+
+        when: "ich ein Ereignis auslöse"
+        ereignisquelle.bewirkt(ZustandWurdeGeändert.of(PAYLOAD))
+
+        then:
+        1 * abonnent1.falls(ZustandWurdeGeändert.of(PAYLOAD))
+        1 * abonnent2.falls(ZustandWurdeGeändert.of(PAYLOAD))
     }
 }
