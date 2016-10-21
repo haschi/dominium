d43f0d307b24510438559e7f685e0e6f9111f909
matthias <matthias.haschka@gmail.com>
2016-10-21 21:36:13 +0200

    Refactoring

--- coding/modelldokumentation/src/main/java/com/github/haschi/modelldokumentation/ContextMap.java
+++ coding/modelldokumentation/src/main/java/com/github/haschi/modelldokumentation/ContextMap.java
@@ -1,10 +1,14 @@
 package com.github.haschi.modelldokumentation;
 
+import com.sun.javadoc.AnnotationDesc;
+import com.sun.javadoc.ClassDoc;
 import com.sun.javadoc.RootDoc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -14,9 +18,9 @@ public class ContextMap
     private static final Logger logger = Logger.getLogger(ContextMap.class.getName());
     private final List<BoundedContext> context = new ArrayList<>();
 
-    static ContextMap create(final RootDoc root)
+    static ContextMap create2(final RootDoc root)
     {
-        final ContextMap cm = new ContextMap();
+        final ContextMap contextMap = new ContextMap();
 
         final List<BoundedContext> boundedContexts = Arrays.stream(root.specifiedPackages())
                 .filter(ContextName::istAbgegrenzterKontext)
@@ -25,13 +29,60 @@ public class ContextMap
 
         for (final BoundedContext boundedContext : boundedContexts)
         {
-            cm.add(boundedContext);
+            contextMap.add(boundedContext);
             logger.log(
                     Level.INFO,
                     String.format("Kontext: %s in %s", boundedContext.getName(), boundedContext.getPackageName()));
         }
 
-        return cm;
+        contextMap.modellErbauen(root);
+
+        return contextMap;
+    }
+
+    private void modellErbauen(final RootDoc root)
+    {
+        final List<Modellelement> modellelements = Arrays.stream(root.classes())
+                .filter(classDoc -> Optional.ofNullable(Arrays.stream(classDoc.annotations())
+                        .reduce(null, ReduceToModellelementannotation(this, classDoc))).isPresent())
+                .map(classDoc -> new Modellelement(
+                        classDoc,
+                        Arrays.stream(classDoc.annotations())
+                                .reduce(null, ReduceToModellelementannotation(this, classDoc))))
+                .collect(Collectors.toList());
+
+        for (final Modellelement modellelement : modellelements)
+        {
+            getModel(modellelement.getClassDoc().containingPackage().name()).getModell()
+                    .get(modellelement.getAnnotationDesc().annotationType().qualifiedName())
+                    .add(modellelement.getDescription());
+        }
+    }
+
+    private static BinaryOperator<AnnotationDesc> ReduceToModellelementannotation(
+            final ContextMap cm, final ClassDoc classDoc)
+    {
+        return (a, b) ->
+        {
+            if (cm.getModel(classDoc.containingPackage().name())
+                    .getModell()
+                    .keySet()
+                    .contains(b.annotationType().qualifiedName()))
+            {
+                if (a == null)
+                {
+                    return b;
+                }
+                else
+                {
+                    throw new IllegalStateException("Mehr als eine Modellannotation gefunden");
+                }
+            }
+            else
+            {
+                return a;
+            }
+        };
     }
 
     public void add(final BoundedContext boundedContext)
@@ -39,7 +90,7 @@ public class ContextMap
         this.context.add(boundedContext);
     }
 
-    public BoundedContext getModel(final String packageName)
+    private BoundedContext getModel(final String packageName)
     {
         for (final BoundedContext boundedContext : this.context)
         {
--- coding/modelldokumentation/src/main/java/com/github/haschi/modelldokumentation/ListClasses.java
+++ coding/modelldokumentation/src/main/java/com/github/haschi/modelldokumentation/ListClasses.java
@@ -1,7 +1,5 @@
 package com.github.haschi.modelldokumentation;
 
-import com.sun.javadoc.AnnotationDesc;
-import com.sun.javadoc.ClassDoc;
 import com.sun.javadoc.RootDoc;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -10,17 +8,13 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.FileWriter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.BinaryOperator;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class ListClasses
 {
     private static final Logger logger = Logger.getLogger(ListClasses.class.getName());
 
+    @SuppressWarnings("unused")
     public static boolean start(final RootDoc root)
     {
         System.out.println("================================");
@@ -33,64 +27,7 @@ public class ListClasses
         }
         System.out.println("================================");
 
-        final ContextMap cm = ContextMap.create(root);
-
-        final List<Modellelement> modellelements = Arrays.stream(root.classes())
-                .filter(classDoc -> Optional.ofNullable(Arrays.stream(classDoc.annotations())
-                        .reduce(null, ReduceToModellelementannotation(cm, classDoc))).isPresent())
-                .map(classDoc -> new Modellelement(
-                        classDoc,
-                        Arrays.stream(classDoc.annotations())
-                                .reduce(null, ReduceToModellelementannotation(cm, classDoc))))
-                .collect(Collectors.toList());
-
-        for (final Modellelement modellelement : modellelements)
-        {
-            final String description = modellelement.getDescription();
-
-            logger.info(modellelement.getClassDoc().name() + " heißt " + description);
-
-            cm.getModel(modellelement.getClassDoc().containingPackage().name())
-                    .getModell()
-                    .get(modellelement.getAnnotationDesc().annotationType().qualifiedName())
-                    .add(modellelement.getDescription());
-        }
-
-        //        Arrays.stream(root.classes()).forEach(classDoc ->
-        //        {
-        //            for (final AnnotationDesc annotation : classDoc.annotations())
-        //            {
-        //                final BoundedContext model = cm.getModel(classDoc.containingPackage().name());
-        //                for (final String modellelement : model.getModell().keySet())
-        //                {
-        //                    final List<String> col = cm.getModel(classDoc.containingPackage().name())
-        //                            .getModell()
-        //                            .get(modellelement);
-        //                    if (annotation.annotationType().qualifiedName().equals(modellelement))
-        //                    {
-        //                        boolean gefunden = false;
-        //                        for (final AnnotationDesc.ElementValuePair elementValuePair : annotation
-        // .elementValues())
-        //                        {
-        //                            if (ContextName.istAnnotationValue(elementValuePair))
-        //                            {
-        //                                final String name = elementValuePair.value().value().toString();
-        //                                col.add(name);
-        //                                gefunden = true;
-        //                            }
-        //                        }
-        //
-        //                        if (!gefunden)
-        //                        {
-        //                            col.add(stream(classDoc.name()
-        //                                    .split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")).collect
-        // (Collectors.joining(
-        //                                    " ")));
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        });
+        final ContextMap cm = ContextMap.create2(root);
 
         for (final BoundedContext c : cm.contexts())
         {
@@ -123,29 +60,4 @@ public class ListClasses
         return true;
     }
 
-    private static BinaryOperator<AnnotationDesc> ReduceToModellelementannotation(
-            final ContextMap cm, final ClassDoc classDoc)
-    {
-        return (a, b) ->
-        {
-            if (cm.getModel(classDoc.containingPackage().name())
-                    .getModell()
-                    .keySet()
-                    .contains(b.annotationType().qualifiedName()))
-            {
-                if (a == null)
-                {
-                    return b;
-                }
-                else
-                {
-                    throw new IllegalStateException("Mehr als eine Modellannotation gefunden");
-                }
-            }
-            else
-            {
-                return a;
-            }
-        };
-    }
 }
