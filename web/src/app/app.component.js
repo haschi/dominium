8a7e6e566ea718c5da7f234b169eab8e286f1cff
matthias <matthias.haschka@gmail.com>
2016-11-21 22:18:28 +0100

    Mit Angular Frontend Stub

---
+++ web/src/app/app.component.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+/*
+ * Angular 2 decorators and services
+ */
+var core_1 = require("@angular/core");
+/*
+ * App Component
+ * Top Level Component
+ */
+var AppComponent = (function () {
+    function AppComponent(appState) {
+        this.appState = appState;
+        this.angularclassLogo = 'assets/img/angularclass-avatar.png';
+        this.name = 'Angular 2 Webpack Starter';
+        this.url = 'https://twitter.com/AngularClass';
+        this.isDarkTheme = false;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        console.log('Initial App State', this.appState.state);
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app',
+            encapsulation: core_1.ViewEncapsulation.None,
+            styleUrls: [
+                './app.component.css',
+                './theme.scss'
+            ],
+            templateUrl: './app.component.html'
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
+//# sourceMappingURL=app.component.js.map
---
+++ web/src/app/app.module.js
@@ -0,0 +1,99 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
+var http_1 = require('@angular/http');
+var router_1 = require('@angular/router');
+var hmr_1 = require('@angularclass/hmr');
+/* Import material design module */
+var material_1 = require('@angular/material');
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+var environment_1 = require('./environment');
+var app_routes_1 = require('./app.routes');
+// App is our top level component
+var app_component_1 = require('./app.component');
+var app_resolver_1 = require('./app.resolver');
+var app_service_1 = require('./app.service');
+var home_1 = require('./home');
+var about_1 = require('./about');
+var no_content_1 = require('./no-content');
+var x_large_1 = require('./home/x-large');
+// Application wide providers
+var APP_PROVIDERS = app_resolver_1.APP_RESOLVER_PROVIDERS.concat([
+    app_service_1.AppState
+]);
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
+var AppModule = (function () {
+    function AppModule(appRef, appState) {
+        this.appRef = appRef;
+        this.appState = appState;
+    }
+    AppModule.prototype.hmrOnInit = function (store) {
+        if (!store || !store.state)
+            return;
+        console.log('HMR store', JSON.stringify(store, null, 2));
+        // set state
+        this.appState._state = store.state;
+        // set input values
+        if ('restoreInputValues' in store) {
+            var restoreInputValues = store.restoreInputValues;
+            setTimeout(restoreInputValues);
+        }
+        this.appRef.tick();
+        delete store.state;
+        delete store.restoreInputValues;
+    };
+    AppModule.prototype.hmrOnDestroy = function (store) {
+        var cmpLocation = this.appRef.components.map(function (cmp) { return cmp.location.nativeElement; });
+        // save state
+        var state = this.appState._state;
+        store.state = state;
+        // recreate root elements
+        store.disposeOldHosts = hmr_1.createNewHosts(cmpLocation);
+        // save input values
+        store.restoreInputValues = hmr_1.createInputTransfer();
+        // remove styles
+        hmr_1.removeNgStyles();
+    };
+    AppModule.prototype.hmrAfterDestroy = function (store) {
+        // display new elements
+        store.disposeOldHosts();
+        delete store.disposeOldHosts;
+    };
+    AppModule = __decorate([
+        core_1.NgModule({
+            bootstrap: [app_component_1.AppComponent],
+            declarations: [
+                app_component_1.AppComponent,
+                about_1.AboutComponent,
+                home_1.HomeComponent,
+                no_content_1.NoContentComponent,
+                x_large_1.XLarge
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                http_1.HttpModule,
+                router_1.RouterModule.forRoot(app_routes_1.ROUTES, { useHash: true }),
+                material_1.MaterialModule.forRoot(),
+            ],
+            providers: [
+                environment_1.ENV_PROVIDERS,
+                APP_PROVIDERS
+            ]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
