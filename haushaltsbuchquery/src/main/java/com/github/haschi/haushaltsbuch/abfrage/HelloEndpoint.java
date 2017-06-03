ddcd11c232cecfae204a8b960137efbe9dc265ac
matthias <matthias.haschka@gmail.com>
2017-06-03 21:22:29 +0200

    Redesign

--- haushaltsbuchquery/src/main/java/com/github/haschi/haushaltsbuch/abfrage/HelloEndpoint.java
+++ haushaltsbuchquery/src/main/java/com/github/haschi/haushaltsbuch/abfrage/HelloEndpoint.java
@@ -1,6 +1,9 @@
 package com.github.haschi.haushaltsbuch.abfrage;
 
+import org.axonframework.config.Configuration;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
@@ -10,9 +13,22 @@ import javax.ws.rs.Produces;
 @Path("/hello")
 public class HelloEndpoint {
 
+    @Inject
+    private Configuration konfiguration;
+
     @GET
     @Produces("text/plain")
     public Response doGet() {
         return Response.ok("Hello from Haushaltsbuch query").build();
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("world")
+    public Response getWorld() {
+        return Response.ok(
+                konfiguration.getModules()
+                    .toArray()
+        ).build();
     }
 }
--- haushaltsbuchquery/src/main/java/com/github/haschi/haushaltsbuch/abfrage/Main.java
+++ haushaltsbuchquery/src/main/java/com/github/haschi/haushaltsbuch/abfrage/Main.java
@@ -4,10 +4,12 @@ import org.wildfly.swarm.Swarm;
 
 public class Main
 {
-    public static void main2(String... args) throws Exception
+    public static void main(String... args) throws Exception
     {
         Swarm swarm = new Swarm(args);
         swarm.start();
         swarm.deploy();
+
+
     }
 }
