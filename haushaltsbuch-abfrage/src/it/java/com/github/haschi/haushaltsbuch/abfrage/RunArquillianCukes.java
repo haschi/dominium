24b03fd8be71928b35daf3896b01ce1f4acebb0d
matthias <matthias.haschka@gmail.com>
2017-06-05 18:35:47 +0200

    Refactoring

--- haushaltsbuch-abfrage/src/it/java/com/github/haschi/haushaltsbuch/abfrage/RunArquillianCukes.java
+++ haushaltsbuch-abfrage/src/it/java/com/github/haschi/haushaltsbuch/abfrage/RunArquillianCukes.java
@@ -1,13 +1,12 @@
 package com.github.haschi.haushaltsbuch.abfrage;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.junit.Test;
 
 // @RunWith(Arquillian.class)
 // @DefaultDeployment
 public class RunArquillianCukes
 {
-    @Test
+    // @Test
     @RunAsClient
     public void alleSzenarienAusführen() throws Throwable
     {
