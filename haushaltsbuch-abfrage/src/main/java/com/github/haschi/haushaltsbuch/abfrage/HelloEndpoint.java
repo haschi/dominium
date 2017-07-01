20da5da69f797b80faedb4d3c36a18d41ffffee9
matthias <matthias.haschka@gmail.com>
2017-07-01 21:17:54 +0200

    Testintegration über gemeinsamen EventStore (Mongo)

--- haushaltsbuch-abfrage/src/main/java/com/github/haschi/haushaltsbuch/abfrage/HelloEndpoint.java
+++ haushaltsbuch-abfrage/src/main/java/com/github/haschi/haushaltsbuch/abfrage/HelloEndpoint.java
@@ -1,8 +1,6 @@
 package com.github.haschi.haushaltsbuch.abfrage;
 
-import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
-import javax.naming.Context;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,33 +10,9 @@ import javax.ws.rs.core.Response;
 @Path("hello")
 public class HelloEndpoint {
 
-//    @Inject
-//    private Configuration konfiguration;
-
-    // @Resource(mappedName = "java:jboss/jgroups/channel/haushaltsbuch-jgroups")
-    // @Resource(lookup = "java:jboss/jgroups/channel/haushaltsbuch")
-    // private JChannel channel;
-
-
-    private Context context;
-
-    // @EJB
-    @Resource(mappedName = "java:module/EineJavaBean")
-    private EineJavaBean bean;
-
     @GET
     @Produces("text/plain")
     public Response doGet() {
         return Response.ok("Hello from Haushaltsbuch query").build();
     }
-
-    //    @GET
-    //    @Produces("text/plain")
-    //    //@Path("world")
-    //    public Response getWorld() throws Exception
-    //    {
-    //        return Response.ok(
-    //                MessageFormat.format("Wert: {0}", bean.getWert()))
-    //                .build();
-    //    }
 }
