1ee6520ebf0f784a32532b9f577f44feb342c19a
matthias <matthias.haschka@gmail.com>
2017-07-09 23:33:00 +0200

    Aufräumen

--- haushaltsbuch-test/src/test/java/com/github/haschi/haushaltsbuch/rest/Ereignismonitor.java
+++ haushaltsbuch-test/src/test/java/com/github/haschi/haushaltsbuch/rest/Ereignismonitor.java
@@ -1,13 +1,10 @@
 package com.github.haschi.haushaltsbuch.rest;
 
 import org.axonframework.eventhandling.EventMessage;
-import org.axonframework.messaging.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.SynchronousQueue;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public final class Ereignismonitor
 {
@@ -15,27 +12,13 @@ public final class Ereignismonitor
 
     private final SynchronousQueue<EventMessage<?>> queue = new SynchronousQueue<>(true);
 
-    public void ereignisEingetreten(final EventMessage<?> any)
+    void ereignisEingetreten(final EventMessage<?> any)
     {
         log.info("Ereignis eingetreten: " + any.getPayloadType().getSimpleName());
         queue.add(any);
     }
 
-    public boolean erwartet(final Predicate<Message<?>> prädikat, final Consumer<InterruptedException> errorHandler)
-    {
-        try
-        {
-            return prädikat.test(queue.take());
-        }
-        catch (final InterruptedException ausnahme)
-        {
-            errorHandler.accept(ausnahme);
-        }
-
-        return false;
-    }
-
-    public Object nächstesEreignis() throws InterruptedException
+    Object nächstesEreignis() throws InterruptedException
     {
         final EventMessage<?> message = queue.take();
         log.info("Nächstes Ereignis: " + message.getPayloadType().getSimpleName());
--- haushaltsbuch-test/src/test/java/com/github/haschi/haushaltsbuch/rest/SynchronisationFehlgeschlagen.java
+++
@@ -1,9 +0,0 @@
-package com.github.haschi.haushaltsbuch.rest;
-
-public class SynchronisationFehlgeschlagen extends RuntimeException
-{
-    public SynchronisationFehlgeschlagen(Exception e)
-    {
-        super(e);
-    }
-}
