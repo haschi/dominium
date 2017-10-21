f16079c80356ed199d7fa572e2ac845ed3c00f57
Matthias Haschla <matthias.haschka@gmail.com>
2017-10-22 01:31:43 +0200

    Frontend lauffähig

--- haushaltsbuch-app/src/app/inventur/inventur.component.spec.ts
+++ haushaltsbuch-app/src/app/inventur/inventur.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InventurComponent } from './inventur.component';
+import { AppMaterialModule } from '../app-material-module';
+import { AppCovalentModuleModule } from '../app-covalent-module.module';
+import { DEMO_APP_ROUTES } from '../routes';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HomeComponent } from '../home/home.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('InventurComponent', () => {
-  let component: InventurComponent;
-  let fixture: ComponentFixture<InventurComponent>;
+    let component: InventurComponent;
+    let fixture: ComponentFixture<InventurComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ InventurComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [InventurComponent, HomeComponent],
+            imports: [
+                NoopAnimationsModule,
+                AppMaterialModule,
+                AppCovalentModuleModule,
+                RouterTestingModule.withRoutes(DEMO_APP_ROUTES)
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InventurComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InventurComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
