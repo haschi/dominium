1e7d90f2d778a28c9af997a059a2eeff8e5133ce
Matthias Haschla <matthias.haschka@gmail.com>
2017-11-24 19:55:54 +0100

    Refactor Invantur Komponente umgestaltet

--- web/src/app/leerer-inhalt/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './leerer-inhalt.component';
--- web/src/meintest.spec.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by matthias on 24.03.17.
- */
-describe('Hello World', () => {
-    it('sollte \'Hello World\' ausgeben', () => {
-        expect(true).toBeTruthy();
-    });
-});
