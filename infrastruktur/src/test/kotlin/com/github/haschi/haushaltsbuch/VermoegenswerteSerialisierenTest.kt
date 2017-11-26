5f532722ea7f0124f14d0e37eea07b561fb6f207
Matthias Haschla <matthias.haschka@gmail.com>
2017-11-26 13:19:35 +0100

    Umstellung Kotlin Zwischenstand

--- infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/VermoegenswerteSerialisierenTest.kt
+++ infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/VermoegenswerteSerialisierenTest.kt
@@ -3,26 +3,52 @@ package com.github.haschi.haushaltsbuch
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
+import org.assertj.core.api.Assertions.assertThatCode
 import org.github.haschi.haushaltsbuch.api.Vermoegenswert
 import org.github.haschi.haushaltsbuch.api.Vermoegenswerte
 import org.github.haschi.haushaltsbuch.api.Währungsbetrag
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
 
 @DisplayName("Vermoegenswerte serialisieren")
 class VermoegenswerteSerialisierenTest
 {
-
-    @Test
-    fun serialisieren()
+    @TestFactory
+    fun serialisieren(): List<DynamicNode>
     {
-        val vermoegenswert = Vermoegenswerte(listOf(
-                Vermoegenswert("Girtokonto", Währungsbetrag.NullEuro),
-                Vermoegenswert("Sparbuch", Währungsbetrag.NullEuro))
-        )
-
         Json.mapper.registerKotlinModule()
-        val json = JsonObject.mapFrom(Vermoegenswerte.keine)
-        println(json.encodePrettily())
+
+        data class Wrapper(val anlagevermoegen: Vermoegenswerte)
+        data class Testfall(val bezeichnung: String, val testwert: Wrapper)
+
+        return listOf(
+                Testfall("2 Vermögenswerte",
+                        Wrapper(
+                                Vermoegenswerte(
+                                        Vermoegenswert(
+                                                "Girtokonto",
+                                                Währungsbetrag.NullEuro),
+                                        Vermoegenswert(
+                                                "Sparbuch",
+                                                Währungsbetrag.NullEuro)
+                                ))),
+                Testfall("1 Vermögenswert",
+                        Wrapper(
+                                Vermoegenswerte(
+                                        Vermoegenswert(
+                                                "Girokonto",
+                                                Währungsbetrag.NullEuro))
+                        )),
+                Testfall("Kein Vermögenswert",
+                        Wrapper(Vermoegenswerte())))
+                .map { testfall ->
+                    dynamicTest(testfall.bezeichnung, {
+                        assertThatCode { JsonObject.mapFrom(testfall.testwert) }
+                                .doesNotThrowAnyException()
+                        println(JsonObject.mapFrom(testfall.testwert).encodePrettily())
+                    })
+                }
     }
 }
--- infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/api/WährungsbetragAddierenTest.kt
+++ infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/api/WährungsbetragAddierenTest.kt
@@ -3,21 +3,48 @@ package com.github.haschi.haushaltsbuch.api
 import org.assertj.core.api.Assertions.assertThat
 import org.github.haschi.haushaltsbuch.api.Währungsbetrag
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 
 @DisplayName("Währungsbetrag addieren")
 class WährungsbetragAddierenTest
 {
-
-    @Test
-    fun währungsbetrag_addieren()
+    @TestFactory
+    fun addieren(): List<DynamicNode>
     {
+        data class Testfall(
+                val x: Währungsbetrag,
+                val y: Währungsbetrag,
+                val summe: Währungsbetrag
+        )
 
-        val x = Währungsbetrag.euro(123.45)
-        val y = Währungsbetrag.euro(1.0)
-
-        val summe: Währungsbetrag = x + y
-
-        assertThat(summe).isEqualTo(Währungsbetrag.euro(124.45))
+        return listOf(
+                Testfall(
+                        Währungsbetrag.NullEuro,
+                        Währungsbetrag.NullEuro,
+                        Währungsbetrag.NullEuro),
+                Testfall(
+                        Währungsbetrag.euro(123.45),
+                        Währungsbetrag.NullEuro,
+                        Währungsbetrag.euro(123.45)),
+                Testfall(
+                        Währungsbetrag.NullEuro,
+                        Währungsbetrag.euro(123.45),
+                        Währungsbetrag.euro(123.45)),
+                Testfall(
+                        Währungsbetrag.euro(42.0),
+                        Währungsbetrag.euro(7.0),
+                        Währungsbetrag.euro(49.0))
+        ).map { testfall ->
+            dynamicTest(
+                    "${testfall.x} + ${testfall.y}",
+                    {
+                        assertThat(testfall.x + testfall.y)
+                                .isEqualTo(testfall.summe)
+                    })
+        }
     }
+
 }
