11061ddb910768d7c77d758a794978baaae4495a
Matthias Haschla <matthias.haschka@gmail.com>
2017-12-02 23:00:35 +0100

    Test Template für die Serialisierung und Deserialisierung von Anweisungen, Ereignissen und Ansichten
    Architektur Refactoring

--- haushaltsbuch/src/main/kotlin/org/github/haschi/haushaltsbuch/projektion/InventarProjektion.kt
+++ haushaltsbuch/src/main/kotlin/org/github/haschi/haushaltsbuch/projektion/InventarProjektion.kt
@@ -2,9 +2,13 @@ package org.github.haschi.haushaltsbuch.projektion
 
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.config.Configuration
+import org.axonframework.eventsourcing.DomainEventMessage
+import org.axonframework.eventsourcing.eventstore.EventStore
 import org.github.haschi.haushaltsbuch.modell.core.events.InventarErfasst
 import org.github.haschi.haushaltsbuch.modell.core.queries.LeseInventar
+import org.github.haschi.haushaltsbuch.modell.core.values.Aggregatkennung
 import org.github.haschi.haushaltsbuch.modell.core.values.Inventar
+import java.util.stream.Stream
 
 class InventarProjektion(private val konfiguration: Configuration)
 {
@@ -12,14 +16,24 @@ class InventarProjektion(private val konfiguration: Configuration)
     @CommandHandler
     fun leseInventar(abfrage: LeseInventar): Inventar
     {
-        val eventStore = konfiguration.eventStore()
-        val eventStream = eventStore.readEvents(abfrage.ausInventur.toString())
+        val stream = EreignisLieferant(konfiguration.eventStore())
+                .ereignisseVon(abfrage.ausInventur)
 
-        return eventStream.asStream()
+        return stream
                 .map { m -> alsInventar(m.payload) }
                 .reduce({ l, r -> if (r == Inventar.leer) l else r })
                 .orElse(Inventar.leer)
 
+    }
+
+    class EreignisLieferant(val eventStore: EventStore){
+
+        fun ereignisseVon(inventur: Aggregatkennung): Stream<out DomainEventMessage<*>>
+        {
+            return eventStore
+                    .readEvents(inventur.toString())
+                    .asStream()
+        }
     }
 
     private fun alsInventar(message: Any): Inventar =
