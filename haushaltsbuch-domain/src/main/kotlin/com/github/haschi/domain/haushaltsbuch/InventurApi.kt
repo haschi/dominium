a7b5ffb5568a2fa3ab6a2a03ef8b62a9dac7fca2
Matthias Haschla <matthias.haschka@gmail.com>
2017-12-06 21:18:29 +0100

    Test Template für die Serialisierung und Deserialisierung von Anweisungen, Ereignissen und Ansichten
    Architektur Refactoring - Update Axon zu Version 3.1

---
+++ haushaltsbuch-domain/src/main/kotlin/com/github/haschi/domain/haushaltsbuch/InventurApi.kt
@@ -0,0 +1,14 @@
+package com.github.haschi.domain.haushaltsbuch
+
+import com.github.haschi.domain.haushaltsbuch.modell.core.commands.BeginneInventur
+import com.github.haschi.domain.haushaltsbuch.modell.core.commands.ErfasseInventar
+import com.github.haschi.domain.haushaltsbuch.modell.core.events.BeendeInventur
+import com.github.haschi.domain.haushaltsbuch.modell.core.values.Aggregatkennung
+import java.util.concurrent.CompletableFuture
+
+interface InventurApi
+{
+    fun send(anweisung: BeginneInventur): CompletableFuture<Aggregatkennung>
+    fun send(anweisung: ErfasseInventar): CompletableFuture<Any>
+    fun send(anweisung: BeendeInventur): CompletableFuture<Any>
+}
