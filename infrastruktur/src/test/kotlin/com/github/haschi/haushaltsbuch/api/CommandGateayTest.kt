e97fbbbda39fee54464c01cce223d8b89fd5b0a1
Matthias Haschla <matthias.haschka@gmail.com>
2017-12-11 07:33:01 +0100

    Architektur Refactoring

--- infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/api/CommandGateayTest.kt
+++ infrastruktur/src/test/kotlin/com/github/haschi/haushaltsbuch/api/CommandGateayTest.kt
@@ -3,7 +3,6 @@ package com.github.haschi.haushaltsbuch.api
 import com.github.haschi.domain.haushaltsbuch.modell.core.commands.BeginneInventur
 import com.github.haschi.domain.haushaltsbuch.modell.core.values.Aggregatkennung
 import com.github.haschi.dominium.haushaltsbuch.core.application.Anwendungskonfiguration
-import com.github.haschi.dominium.haushaltsbuch.core.application.Dominium
 import com.github.haschi.haushaltsbuch.infrastruktur.AxonInfrastrukturFactory
 import io.reactivex.Observable
 import org.junit.jupiter.api.Test
