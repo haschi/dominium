b5707bfdb84204e27f24d43180e2ca506c9134be
Matthias Haschla <matthias.haschka@gmail.com>
2017-12-27 15:28:16 +0100

    Verbesserung des Command Gateways

--- frontend/src/main/src/app/shared/command-gateway/command-gateway-epics.service.ts
+++ frontend/src/main/src/app/shared/command-gateway/command-gateway-epics.service.ts
@@ -13,11 +13,6 @@ import {
     CommandGatewayActionsService,
     CommandGatewayActionType
 } from './command-gateway-actions.service';
-import { Actions } from '../../store/actions';
-
-const isCommandMessage = (type: string) =>
-    (action: CommandMessageAction): boolean =>
-        action.message.type === type;
 
 @Injectable()
 export class CommandGatewayEpicsService {
@@ -25,29 +20,21 @@ export class CommandGatewayEpicsService {
                 private service: CommandGatewayService) {
     }
 
-    public createEpic(commandType: string): [EpicMiddleware<CommandAction, AppState>] {
+    public createEpic(): [EpicMiddleware<CommandAction, AppState>] {
         return [
-            createEpicMiddleware(this.createAngefordertEpic(commandType)),
-            createEpicMiddleware(this.createGelungenEpic())
+            createEpicMiddleware(this.createAngefordertEpic())
         ];
     }
 
     // angefordert -> gelungen
     // angefordert -> fehlgeschlagen
-    private createAngefordertEpic(commandType: string): Epic<CommandAction, AppState> {
+    private createAngefordertEpic(): Epic<CommandAction, AppState> {
         return (action$, store) => action$
             .ofType(CommandGatewayActionType.angefordert)
-            .do(action => console.info("EXECUTE COMMAND EPIC: " + JSON.stringify(action)))
+            .do(action => console.info("EXECUTE EPIC: " + JSON.stringify(action)))
             .mergeMap(action => this.service.post(action as CommandMessageAction)
                 .map(response => this.aktionen.gelungen(action.message, response)))
 
         // TODO: Fehlerbehandlung!
-    }
-
-    private createGelungenEpic(): Epic<CommandMessageAction, AppState> {
-        return (action$, store) => action$
-            .ofType(CommandGatewayActionType.gelungen)
-            .filter(message => isCommandMessage('com.github.haschi.dominium.haushaltsbuch.core.model.commands.BeginneInventur')(message))
-            .map(message => ({type: Actions.InventurBegonnen, message: message.message}))
     }
 }
