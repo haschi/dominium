fe1ef0cc0dce35033bf870a6d4cc016dfdcdce25
Matthias Haschla <matthias.haschka@gmail.com>
2018-01-21 14:04:56 +0100

    Erstellung der ReST Endpoints durch Swagger Codegen

--- ui/backend/src/main/kotlin/com/github/haschi/haushaltsbuch/backend/rest/api/Application.kt
+++ ui/backend/src/main/kotlin/com/github/haschi/haushaltsbuch/backend/rest/api/Application.kt
@@ -1,15 +1,18 @@
-package com.github.haschi.haushaltsbuch.haushaltsbuch.backend
+package com.github.haschi.haushaltsbuch.backend.rest.api
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan(
+        basePackages = arrayOf("com.github.haschi.haushaltsbuch"),
+        basePackageClasses = arrayOf(Version2Endpoint::class))
 class Application
 
 fun main(args: Array<String>) {
