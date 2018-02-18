74b3fa218d0b9a71801794bf477bcb3b42ba990d
Matthias Haschla <matthias.haschka@gmail.com>
2018-02-18 16:31:20 +0100

    Test der Log-Ausgabe

--- ui/backend/src/test/kotlin/com/github/haschi/haushaltsbuch/backend/GestarteteAnwendungTest.kt
+++ ui/backend/src/test/kotlin/com/github/haschi/haushaltsbuch/backend/GestarteteAnwendungTest.kt
@@ -1,13 +1,17 @@
 package com.github.haschi.haushaltsbuch.backend
 
 import com.github.haschi.dominium.haushaltsbuch.core.application.Anwendung
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class GestarteteAnwendungTest
 {
-
     @Test
     fun `sollte Anwendungskern beenden`()
     {
@@ -17,5 +21,50 @@ class GestarteteAnwendungTest
         gestarteteAnwendung.shutdown()
 
         verify(anwendung).stop()
+    }
+
+    @BeforeEach
+    fun ausgabeKlauen()
+    {
+        System.setOut(PrintStream(output))
+    }
+
+    @Test
+    fun `sollte die Log-Ausgabe 'Anwendungskern wird beendet' beim Beenden ausgeben`()
+    {
+        val anwendung = mock(Anwendung::class.java)
+
+        val sut = GestarteteAnwendung(anwendung)
+        sut.shutdown()
+
+        assertThat(output.toString())
+                .contains("Anwendungskern wird beendet")
+
+        assertThat(output.toString())
+                .contains("ENDE")
+    }
+
+    @Test
+    fun `sollte die Log-Ausgabe 'ENDE' beim Beenden ausgeben`()
+    {
+        val anwendung = mock(Anwendung::class.java)
+
+        val sut = GestarteteAnwendung(anwendung)
+        sut.shutdown()
+
+        assertThat(output.toString())
+                .contains("ENDE")
+    }
+
+
+    @AfterEach
+    fun ausgabeZurueckgeben()
+    {
+        System.setOut(System.out)
+    }
+
+    companion object
+    {
+        val output = ByteArrayOutputStream()
     }
 }
