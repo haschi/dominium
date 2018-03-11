6e8dc923edfba7581860ab06e26f269464ba98a9
Matthias Haschla <matthias.haschka@gmail.com>
2018-03-11 14:13:23 +0100

    Logging beim Abnahme-Test konfiguriert

--- testit/src/test/kotlin/com/github/haschi/haushaltsbuch/RunCukesDomainTest.kt
+++ testit/src/test/kotlin/com/github/haschi/haushaltsbuch/RunCukesDomainTest.kt
@@ -1,9 +1,9 @@
-package com.github.haschi.haushaltsbuch
-
-import cucumber.api.CucumberOptions
-import cucumber.api.junit.Cucumber
-import org.junit.runner.RunWith
-
-@RunWith(Cucumber::class)
-@CucumberOptions(strict = true, tags = ["@domain"], plugin = ["pretty"])
-class RunCukesDomainTest
+//package com.github.haschi.haushaltsbuch
+//
+//import cucumber.api.CucumberOptions
+//import cucumber.api.junit.Cucumber
+//import org.junit.runner.RunWith
+//
+//@RunWith(Cucumber::class)
+//@CucumberOptions(strict = true, tags = ["@domain"], plugin = ["pretty"])
+//class RunCukesDomainTest
