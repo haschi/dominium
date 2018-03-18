74748afe85c8e078c5112d23cb700833d33c53d7
Matthias Haschla <matthias.haschka@gmail.com>
2018-03-18 14:17:27 +0100

    dominium-33 Logging verbessern Neu: CommandMessageLoggingInterceptor

--- core/src/main/kotlin/com/github/haschi/dominium/haushaltsbuch/core/application/Anwendungskonfiguration.kt
+++ core/src/main/kotlin/com/github/haschi/dominium/haushaltsbuch/core/application/Anwendungskonfiguration.kt
@@ -4,6 +4,8 @@ import com.github.haschi.dominium.haushaltsbuch.core.domain.Historie
 import com.github.haschi.dominium.haushaltsbuch.core.domain.InventarProjektion
 import com.github.haschi.dominium.haushaltsbuch.core.model.Haushaltsbuch
 import com.github.haschi.dominium.haushaltsbuch.core.model.Inventur
+import org.axonframework.commandhandling.CommandBus
+import org.axonframework.commandhandling.SimpleCommandBus
 import org.axonframework.config.Configuration
 import org.axonframework.config.Configurer
 import org.axonframework.config.DefaultConfigurer
@@ -14,6 +16,7 @@ open class Anwendungskonfiguration(private val infrastruktur: Infrastrukturfabri
 
     private val konfiguration: Configuration = DefaultConfigurer.defaultConfiguration()
             .configureLogger()
+            .configureCommandBus(infrastruktur::commandBus)
             .configureAggregate(Inventur::class.java)
             .configureAggregate(Haushaltsbuch::class.java)
             .registerComponent(Historie::class.java, infrastruktur::historie)
--- infrastruktur/src/main/kotlin/com/github/haschi/haushaltsbuch/infrastruktur/AxonInfrastrukturFactory.kt
+++ infrastruktur/src/main/kotlin/com/github/haschi/haushaltsbuch/infrastruktur/AxonInfrastrukturFactory.kt
@@ -2,16 +2,27 @@ package com.github.haschi.haushaltsbuch.infrastruktur
 
 import com.github.haschi.dominium.haushaltsbuch.core.application.Infrastrukturfabrik
 import com.github.haschi.dominium.haushaltsbuch.core.domain.Historie
+import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.CommandCallback
+import org.axonframework.commandhandling.SimpleCommandBus
 import org.axonframework.config.Configuration
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
 import org.axonframework.eventsourcing.eventstore.EventStore
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
 import org.axonframework.messaging.Message
+import org.axonframework.messaging.interceptors.CorrelationDataInterceptor
 import org.axonframework.monitoring.MessageMonitor
 
 class AxonInfrastrukturFactory : Infrastrukturfabrik
 {
+    override fun commandBus(konfiguration: Configuration): CommandBus
+    {
+        val commandBus = SimpleCommandBus()
+        commandBus.registerHandlerInterceptor(CorrelationDataInterceptor(konfiguration.correlationDataProviders()));
+        commandBus.registerHandlerInterceptor(CommandLoggingInterceptor)
+        return commandBus
+    }
+
     override fun logger(): CommandCallback<Any, Any> =
         LoggingCallback.INSTANCE
 
